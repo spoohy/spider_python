905844f816acdeff69e3f7a88ce84fbc2a7117af
shawn <shy3254@gmail.com>
2016-06-21 15:53:29 +0800

    Add files via upload

---
+++ html_downloader.py
@@ -0,0 +1,13 @@
+import urllib2
+
+__author__ = 'Administrator'
+#coding:utf8
+
+class HtmlDownloader(object):
+    def download(self, url):
+        if url is None:
+            return None
+        response = urllib2.urlopen(url)
+        if response.getcode() != 200:
+            return None
+        return response.read()
